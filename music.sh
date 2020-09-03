474ca0ae70bed649737795915a97c12f3d80ab35
Crypticwave <abhinavdatta38@gmail.com>
2020-09-03 16:15:44 +0530

    UPdated music script

--- music.sh
+++ music.sh
@@ -1,5 +1,5 @@
 #echo "$(ls ~/Music/All/)"| rofi -dmenu | xargs -r -I {} echo {} | sed 's/.mp3//g' > song.info
-echo "$(ls ~/Music/All/)"| rofi -dmenu  | sed 's/.mp3//g' > song.info
-notify-send "Now Playing: $(cat song.info)" -t 3000
+echo "$(ls ~/Music/All/ | sed 's/.mp3//g')"| rofi -dmenu > song.info
+notify-send "$(cat song.info)" 
 play ~/Music/All/$(cat song.info).mp3
 echo "Nothing Playing" > song.info
