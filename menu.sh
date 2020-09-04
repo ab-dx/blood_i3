ad0c9e334bff13e8d115d91fef805ca7d79f6e63
Crypticwave <abhinavdatta38@gmail.com>
2020-09-04 11:57:36 +0530

    Improved menu script

--- menu.sh
+++ menu.sh
@@ -1,9 +1,9 @@
 #printf "  " && echo $(curl wttr.in ) | awk '{printf $8 }' && printf " "
 #weather requires weather-util
-printf "  " &&  weather VOBG | awk 'NR == 8' | awk '{printf $2 }' | sed 's/\([a-z]\)\([a-z]*\)/\U\1\L\2/g' | sed 's/;//g' && printf " "
-weather VOBG | awk 'NR == 8' | awk '{printf $3 }' | sed 's/;//g' && printf " "
+printf " " &&  weather VOBG | awk 'NR == 8' | sed 's/Sky //g' | sed 's/conditions: //g' | sed 's/Weather: //g' | sed 's/\([a-z]\)\([a-z]*\)/\U\1\L\2/g' | sed 's/;//g' && printf " "
+#weather VOBG | awk 'NR == 8' | awk '{printf $3 }' | sed 's/;//g' && printf " "
 #echo $(curl wttr.in ) | awk '{printf $9}'  
-echo
+#echo
 echo
 echo $(date '+ %H:%M' )
 echo $(echo -n "  \t" && acpi | awk '{print $4} ')
